ed33e12fe058792a9c940f589bacaf6c2b4ad107
Mani <mani@cozyvision.com>
2021-12-23 17:59:22 +0530

    API Request Method

--- index.js
+++ index.js
@@ -25,16 +25,25 @@ class SMS {  // Main class
     if (!text || typeof text != 'string') {
       throw new TypeError('Second argument text is required, it must be string')
     }
+	
+	const formData = {	
+			  user: this.user,
+			  pwd: this.pwd,
+			  sender: senderid,
+			  mobileno: phoneNumbers,
+			  text: text
+	};
 
     try {
       const response = await request({
-        method: 'GET',
-        uri: 'https://www.smsalert.co.in/api/push.json?user=' + this.user + '&pwd=' + this.pwd + '&sender=' + senderid + '&mobileno=' + phoneNumbers + '&text=' + text,
-        json: true
+        method: 'POST',
+        uri: 'https://www.smsalert.co.in/api/push.json',
+        json: true,
+		form: formData
       }).then((value) => {
         console.log(value);
         return value;
-      });
+	   });
       return response
     } catch (err) {
       throw err
